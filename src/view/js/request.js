06e1d4fb4022a2beb37aa9204b7ff5173c1784a2
lucas-miyazaki <lucasmiyazaki6@gmail.com>
2023-12-01 11:39:58 -0300

    first commit

--- src/view/js/request.js
+++ src/view/js/request.js
@@ -58,54 +58,54 @@ function cadastrar() {
         },
         body: JSON.stringify(dados)
     })
-    .then(response => response.json())
-    .then(resposta => {
-        console.log('Resposta do servidor:', resposta);
-    })
-    .catch(error => {
-        console.error('Erro ao enviar dados para o servidor:', error);
-    });
+        .then(response => response.json())
+        .then(resposta => {
+            console.log('Resposta do servidor:', resposta);
+        })
+        .catch(error => {
+            console.error('Erro ao enviar dados para o servidor:', error);
+        });
 }
 
 function listarVeiculo() {
     fetch(`${url_server}/veiculo`)
-    .then(response => response.json())
-    .then(data => {
-        const tabela = document.querySelector('table');
+        .then(response => response.json())
+        .then(data => {
+            const tabela = document.querySelector('table');
 
-        data.forEach(veiculo => {
-            const elementTr = document.createElement('tr');
-            const tdTipo = document.createElement('td');
-            const tdMarca = document.createElement('td');
-            const tdCategoria = document.createElement('td');
-            const tdCombustivel = document.createElement('td');
-            const tdPlaca = document.createElement('td');
-            const tdCor = document.createElement('td');
-            const tdQuantEixos = document.createElement('td');
-            const tdAltura = document.createElement('td');
-            const tdPeso = document.createElement('td');
+            data.forEach(veiculo => {
+                const elementTr = document.createElement('tr');
+                const tdTipo = document.createElement('td');
+                const tdMarca = document.createElement('td');
+                const tdCategoria = document.createElement('td');
+                const tdCombustivel = document.createElement('td');
+                const tdPlaca = document.createElement('td');
+                const tdCor = document.createElement('td');
+                const tdQuantEixos = document.createElement('td');
+                const tdAltura = document.createElement('td');
+                const tdPeso = document.createElement('td');
 
-            tdTipo.textContent = veiculo.tipo;
-            tdMarca.textContent = veiculo.marca;
-            tdCategoria.textContent = veiculo.categoria;
-            tdCombustivel.textContent = veiculo.combustivel;
-            tdPlaca.textContent = veiculo.placa;
-            tdCor.textContent = veiculo.cor;
-            tdQuantEixos.textContent = veiculo.quantEixos;
-            tdAltura.textContent = veiculo.altura;
-            tdPeso.textContent = veiculo.peso;
+                tdTipo.textContent = veiculo.tipo;
+                tdMarca.textContent = veiculo.marca;
+                tdCategoria.textContent = veiculo.categoria;
+                tdCombustivel.textContent = veiculo.combustivel;
+                tdPlaca.textContent = veiculo.placa;
+                tdCor.textContent = veiculo.cor;
+                tdQuantEixos.textContent = veiculo.quantEixos;
+                tdAltura.textContent = veiculo.altura;
+                tdPeso.textContent = veiculo.peso;
 
-            elementTr.appendChild(tdTipo);
-            elementTr.appendChild(tdMarca);
-            elementTr.appendChild(tdCategoria);
-            elementTr.appendChild(tdCombustivel);
-            elementTr.appendChild(tdPlaca);
-            elementTr.appendChild(tdCor);
-            elementTr.appendChild(tdQuantEixos);
-            elementTr.appendChild(tdAltura);
-            elementTr.appendChild(tdPeso);
+                elementTr.appendChild(tdTipo);
+                elementTr.appendChild(tdMarca);
+                elementTr.appendChild(tdCategoria);
+                elementTr.appendChild(tdCombustivel);
+                elementTr.appendChild(tdPlaca);
+                elementTr.appendChild(tdCor);
+                elementTr.appendChild(tdQuantEixos);
+                elementTr.appendChild(tdAltura);
+                elementTr.appendChild(tdPeso);
 
-            tabela.appendChild(elementTr);
+                tabela.appendChild(elementTr);
+            });
         });
-    });
 }
